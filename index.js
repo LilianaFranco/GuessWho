14ffa592f1c2b94997cce5ccfa90e8e6df957ec1
LilianaFranco <franco0123@gmail.com>
2020-05-13 19:01:30 -0500

    Style sheet for both html pages. No modal yet

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const startButton = document.getElementById('start');
 const playerInfo = document.getElementById('player-info');
+const changePlayerButton = document.getElementById('change-player');
 
 startButton.addEventListener('click', function(){
 	let name = playerInfo.elements[0].value;
 	let email = playerInfo.elements[1].value;
 	window.location.href='./game.html';
+});
+
+changePlayerButton.addEventListener('click', function(){
+	window.location.href='./index.html';
 });
 
 console.log("name");
