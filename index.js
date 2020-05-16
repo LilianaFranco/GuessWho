b3c2c1cb89fc5d0c351b05ee60f9b0ad5df48a5f
LilianaFranco <franco0123@gmail.com>
2020-05-16 14:23:30 -0500

    Styles with grid in CSS and structure of the modal

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const startButton = document.getElementById('start');
 const playerInfo = document.getElementById('player-info');
 const changePlayerButton = document.getElementById('change-player');
+const guessButton = document.getElementById('guess');
+let modal = document.getElementById('modal');
 
 startButton.addEventListener('click', function(){
 	let name = playerInfo.elements[0].value;
 	let email = playerInfo.elements[1].value;
-	window.location.href='./game.html';
+	window.location.href= './game.html';
 });
+
 
 changePlayerButton.addEventListener('click', function(){
 	window.location.href='./index.html';
 });
 
-console.log("name");
+guessButton.addEventListener('click', function(){
+	console.log("abrir modal");
+	modalStyle.removeProperty("display");
+});
 
+guessButton.addEventListener('click', function(){
+	console.log('cerrar modal');
+	modal.style.removeProperty('display');	
+});
