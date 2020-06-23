b33685daf05a88ddeed2106ca5eee4e19e1317d0
LilianaFranco <franco0123@gmail.com>
2020-06-22 20:35:36 -0500

    One HTML structure for the game, button actions to hide pages and changing HTML text with JS

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const startButton = document.getElementById('start');
+const mainPage = document.getElementsByClassName("index-page");
+const gamePage = document.getElementsByClassName("game-page");
 const playerInfo = document.getElementById('player-info');
 const changePlayerButton = document.getElementById('change-player');
 const guessButton = document.getElementById('guess');
@@ -7,16 +9,18 @@ let modal = document.getElementById('modal');
 startButton.addEventListener('click', function(){
 	let name = playerInfo.elements[0].value;
 	let email = playerInfo.elements[1].value;
-	window.location.href= './game.html';
+	mainPage[0].style.display = "none";	
+	gamePage[0].style.display = "block";	
+	document.getElementById("user-name").innerHTML = name;
+	
 });
-
 
 changePlayerButton.addEventListener('click', function(){
 	window.location.href='./index.html';
 });
 
 guessButton.addEventListener('click', function(){
-	console.log("abrir modal");
+	console.log('abrir modal');
 	modalStyle.removeProperty("display");
 });
 
